77c0429558fba7f81b3306813898a5763f8bc998
Bagusarya <aryabagus453@gmail.com>
2025-06-07 20:01:46 +0700

    add latest build

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  base: 'https://test-production-6cd6.up.railway.app/build/',
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
