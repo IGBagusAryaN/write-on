d280cf0175cfe2b7fb247ff39367dea7e32f5003
Bagusarya <aryabagus453@gmail.com>
2025-06-08 15:52:21 +0700

    new

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,10 +1,5 @@
 <?php
 
 return [
-    'cloud' => [
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key'    => env('CLOUDINARY_API_KEY'),
-        'api_secret' => env('CLOUDINARY_API_SECRET'),
-    ],
-    'url' => env('CLOUDINARY_URL'),
+    'cloud_url' => env('CLOUDINARY_URL'),
 ];
